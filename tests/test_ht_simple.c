b376f9c20f5f862fdfb3627a68fab16011e48c20
Robin Martinjak <rob@rmartinjak.de>
2012-03-28 08:01:31 +0200

    added more hashtale tests

--- tests/test_ht_simple.c
+++ tests/test_ht_simple.c
@@ -30,6 +30,32 @@ START_TEST (test_ht_insert)
 }
 END_TEST
 
+START_TEST (test_ht_insert_null)
+{
+    int res, data;
+    void *p;
+
+    res = ht_insert(ht, "test", NULL);
+    fail_unless(res == HT_OK,
+        "inserting a new item with NULL as data should return HT_OK");
+
+    p = ht_get(ht, "test");
+    fail_unless(p == NULL,
+        "ht_get() should return the NULL inserted by ht_insert()");
+}
+END_TEST
+
+START_TEST (test_ht_insert_remove)
+{
+    ht_insert(ht, "test", NULL);
+    ht_remove(ht, "test");
+
+    fail_unless(ht_empty(ht),
+        "after inserting and removing an item with the same key to an empty"
+        "hashtable, it should be empty again");
+}
+END_TEST
+
 START_TEST (test_ht_set)
 {
     int res, data;
@@ -74,6 +100,7 @@ START_TEST (test_ht_get_nonexistent)
 }
 END_TEST
 
+
 Suite *ht_simple_suite(void)
 {
     Suite *s = suite_create("hashtable operations with small amounts of (static) data");
@@ -83,9 +110,12 @@ Suite *ht_simple_suite(void)
     tcase_add_checked_fixture (tc_simple, setup, teardown);
 
     tcase_add_test(tc_simple, test_ht_insert);
+    tcase_add_test(tc_simple, test_ht_insert_null);
+    tcase_add_test(tc_simple, test_ht_insert_remove);
     tcase_add_test(tc_simple, test_ht_set);
     tcase_add_test(tc_simple, test_ht_set_twice);
     tcase_add_test(tc_simple, test_ht_get_nonexistent);
+
 
     suite_add_tcase(s, tc_simple);
 
